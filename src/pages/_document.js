bb11cb85364cbe654e91a57b1874f6a658bf23a7
Capt Nork <138743746+norkart1@users.noreply.github.com>
2024-09-05 16:11:21 +0530

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,7 +6,7 @@ export default function Document() {
       <Head>
       <link rel="icon" href="/qul-favicon.png" />
       <link rel="apple-touch-icon" sizes="180x180" href="/qul-favicon.png" />
-      <title>Qul Certificate Search</title>
+      <title>Maerika2k24 - Results </title>
       </Head>
       <body>
         <Main />
