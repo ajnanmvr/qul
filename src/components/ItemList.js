ba64acb8eed8bffab163a969fb2ad4589474999c
Capt Nork <138743746+norkart1@users.noreply.github.com>
2024-10-09 14:59:30 +0530

    Update ItemList.js

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,11 +1,10 @@
-
-import { saveAs } from 'file-saver';
-import { jsPDF } from 'jspdf';
 import { useState } from 'react';
 
 export default function ItemList({ items, searchQuery, maxSuggestions }) {
   const [selectedItem, setSelectedItem] = useState(null);
   const [showAllSuggestions, setShowAllSuggestions] = useState(false);
+  const [likeCount, setLikeCount] = useState(0);
+  const [dislikeCount, setDislikeCount] = useState(0);
 
   const uniqueItems = new Set();
   const filteredItems = [];
@@ -19,56 +18,6 @@ export default function ItemList({ items, searchQuery, maxSuggestions }) {
       filteredItems.push(item);
     }
   });
-  const handleDownloadPDF = (item) => {
-    const doc = new jsPDF('landscape', 'mm', 'a4');
-
-    // Load Montserrat font
-    doc.addFont('https://fonts.gstatic.com/s/montserrat/v15/JTUSjIg1_i6t8kCHKm459Wlhzg.ttfhttps://fonts.googleapis.com/css2?family=SUSE:wght@100..800&display=swap" rel="stylesheet','normal');
-
-    const backgroundImageUrl = '/certificate.jpg'; // Update the path to your background image
-
-    const pdfWidth = doc.internal.pageSize.getWidth();
-    const pdfHeight = doc.internal.pageSize.getHeight();
-
-    fetch(backgroundImageUrl)
-      .then((response) => response.blob())
-      .then((blob) => {
-        const imgData = URL.createObjectURL(blob);
-
-        // Add the background image
-        doc.addImage(imgData, 'JPEG', 0, 0, pdfWidth, pdfHeight);
-
-        // Set the font to Montserrat
-        doc.setFont('Montserrat');
-
-        // Add text and other content on top of the background image
-        doc.setFontSize(15);
-        doc.setTextColor(0, 0, 0); // Set text color to black
-
-        const capitalizedName = item.name.toUpperCase();
-        const capitalizedGrade = item.grade ? item.grade.toUpperCase() : '';
-        const capitalizedPosition = item.position ? item.position.toUpperCase() : '';
-        const capitalizedProgramme = item.programme.toUpperCase();
-
-        doc.text(`This is to certify that Mr. ${capitalizedName} has been awarded`, pdfWidth / 2, 135, { align: 'center' });
-
-        if (item.position && item.grade) {
-          doc.text(`the ${capitalizedPosition} in ${capitalizedProgramme} in MAERIKA 2K24`, pdfWidth / 2, 144, { align: 'center' });
-        } else if  (item.position) {
-          doc.text(`the ${capitalizedPosition} in ${capitalizedProgramme} in MAERIKA 2K24`, pdfWidth / 2, 144, { align: 'center' });
-        } else if (item.grade){
-          doc.text(`the ${capitalizedPosition} in ${capitalizedProgramme} in MAERIKA 2K24`, pdfWidth / 2, 144, { align: 'center' });
-        }
-
-        doc.text(`Arts Fest organized by Jawharathul Uloom Suffa Dars Students' Association (JDSA)`, pdfWidth / 2, 153, { align: 'center' });
-        doc.text(`.`, pdfWidth / 2, 162, { align: 'center' });
-
-        const pdfBlob = doc.output('blob');
-        saveAs(pdfBlob, `${item.programme} - ${item.name}.pdf`);
-      });
-  };
-
-
 
   const handleItemClick = (item) => {
     setSelectedItem(item);
@@ -82,21 +31,29 @@ export default function ItemList({ items, searchQuery, maxSuggestions }) {
     setShowAllSuggestions(true);
   };
 
+  const handleLike = () => {
+    setLikeCount((prevCount) => prevCount + 1);
+  };
+
+  const handleDislike = () => {
+    setDislikeCount((prevCount) => prevCount + 1);
+  };
+
   return (
-    <ul className='flex justify-center mt-4 flex-wrap'>
+    <ul className="flex justify-center mt-4 flex-wrap">
       {(showAllSuggestions
         ? filteredItems
         : filteredItems.slice(0, maxSuggestions)
       ).map((item) => (
         <li
           key={item.name}
-          className="cursor-pointer hover:bg-green-900  py-2 px-4 bg-green-800 font-semibold text-white rounded-full m-1"
+          className="cursor-pointer hover:bg-green-900 py-2 px-4 bg-green-800 font-semibold text-white rounded-full m-1"
           onClick={() => handleItemClick(item)}
         >
           <h2>{item.name}</h2>
         </li>
       ))}
-<br/>
+      <br />
       {!showAllSuggestions && (
         <li>
           <button
@@ -109,7 +66,7 @@ export default function ItemList({ items, searchQuery, maxSuggestions }) {
       )}
       {selectedItem && (
         <div className="fixed top-0 left-0 w-full h-full bg-gray-100 bg-opacity-75 flex justify-center items-center z-50">
-          <div className="bg-white p-6 rounded-xl shadow-xl max-w-md w-full  border-green-800">
+          <div className="bg-white p-6 rounded-xl shadow-xl max-w-md w-full border-green-800">
             <h2 className="text-2xl font-bold text-center mb-9">{selectedItem.name}</h2>
             <div className="space-y-2">
               {items
@@ -121,36 +78,51 @@ export default function ItemList({ items, searchQuery, maxSuggestions }) {
                       <p className="text-gray-800 font-bold">{item.programme}</p>
                       {item.position && item.grade ? (
                         <p className="text-gray-800">
-                          <span className="font-semibold  p-0.5 pl-2 pr-2 bg-slate-200 rounded-md">{item.position}</span> with{' '}
-                          <span className="font-semibold  p-0.5 pl-2 pr-2 bg-slate-200 rounded-md">{item.grade}</span> grade
+                          <span className="font-semibold p-0.5 pl-2 pr-2 bg-slate-200 rounded-md">{item.position}</span> with{' '}
+                          <span className="font-semibold p-0.5 pl-2 pr-2 bg-slate-200 rounded-md">{item.grade}</span> grade
                         </p>
                       ) : (
                         <>
                           {item.position && (
                             <p className="text-gray-800">
-                              Position: <span className="font-semibold  p-0.5 pl-2 pr-2 bg-slate-200 rounded-md">{item.position}</span>
+                              Position: <span className="font-semibold p-0.5 pl-2 pr-2 bg-slate-200 rounded-md">{item.position}</span>
                             </p>
                           )}
                           {item.grade && (
                             <p className="text-gray-800">
-                              Grade: <span className="font-semibold  p-0.5 pl-2 pr-2 bg-slate-200 rounded-md">{item.grade}</span>
+                              Grade: <span className="font-semibold p-0.5 pl-2 pr-2 bg-slate-200 rounded-md">{item.grade}</span>
                             </p>
                           )}
                         </>
                       )}
                     </div>
-                    <button
-                      onClick={() => handleDownloadPDF(item)}
-                      className="px-4 py-2 bg-yellow-500 text-black font-semibold rounded-md ml-auto  hover:bg-yellow-600 hover:text-black"
-                    >
-                      Download PDF
-                    </button>
+                    <div className="ml-auto flex items-center">
+                      <button
+                        onClick={handleLike}
+                        className="px-4 py-2 bg-green-500 text-white font-semibold rounded-md ml-2 hover:bg-green-600"
+                      >
+                        Like
+                      </button>
+                      <button
+                        onClick={handleDislike}
+                        className="px-4 py-2 bg-red-500 text-white font-semibold rounded-md ml-2 hover:bg-red-600"
+                      >
+                        Dislike
+                      </button>
+                    </div>
                   </div>
                 ))}
             </div>
+            <div className="text-center mt-4">
+              <p className="text-sm text-zinc-500">
+                Likes: {likeCount} | Dislikes: {dislikeCount}
+              </p>
+            </div>
             <div className="flex justify-center mt-4 flex-col">
-              <p className="text-zinc-500 text-sm text-center m-5">Designed and Developed by <br/>
-               <span className="font-semibold text-lg">JDSA Media Wing</span></p>
+              <p className="text-zinc-500 text-sm text-center m-5">
+                Designed and Developed by <br />
+                <span className="font-semibold text-lg">JDSA Media Wing</span>
+              </p>
               <button
                 onClick={handleCloseModal}
                 className="px-4 py-2 bg-green-800 text-white font-bold rounded-md hover:bg-green-900"
